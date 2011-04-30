4734e5e41dd1bddf6eeead38e9e65e952d900133
Lachie Cox <lachiec@gmail.com>
2011-04-30 16:15:53 +1000

    show sha

--- home/.oh-my-zsh/lib/git.zsh
+++ home/.oh-my-zsh/lib/git.zsh
@@ -1,7 +1,8 @@
 # get the name of the branch we are on
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  sha=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}@${sha}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
 # Checks if working tree is dirty
