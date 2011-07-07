feb852643b191ebe5b8101001d99eb00cf6210e7
Lachie Cox <lachiec@gmail.com>
2011-07-07 17:37:52 +1000

    this should fix my vim crashes

--- home/.oh-my-zsh/custom/editor.zsh
+++ home/.oh-my-zsh/custom/editor.zsh
@@ -1,1 +1,8 @@
 export EDITOR=/usr/bin/vim
+
+function mvim()
+{
+  # spawn a sub-shell, unset environment variables for the sub-shell, start mvim. 
+  (unset GEM_PATH; unset GEM_HOME; command mvim "$@")
+}
+
