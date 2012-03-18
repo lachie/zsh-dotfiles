50f2758b06cab5a83cde28f87170a6763a8f021b
Lachie Cox <lachiec@gmail.com>
2012-03-19 08:44:26 +1100

    vi mode

--- home/.oh-my-zsh/lib/key-bindings.zsh
+++ home/.oh-my-zsh/lib/key-bindings.zsh
@@ -1,24 +1,28 @@
 # TODO: Explain what some of this does..
 
-bindkey -e
-bindkey '\ew' kill-region
-bindkey -s '\el' "ls\n"
-bindkey -s '\e.' "..\n"
-bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
+bindkey -v
 
-# make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+#bindkey '\ew' kill-region
+#bindkey -s '\el' "ls\n"
+#bindkey -s '\e.' "..\n"
+#bindkey '^r' history-incremental-search-backward
+#bindkey "^[[5~" up-line-or-history
+#bindkey "^[[6~" down-line-or-history
 
-bindkey "^[[H" beginning-of-line
-bindkey "^[[1~" beginning-of-line
-bindkey "^[[F"  end-of-line
-bindkey "^[[4~" end-of-line
-bindkey ' ' magic-space    # also do history expansion on space
+## make search up and down work, so partially type and hit up/down to find relevant stuff
+#bindkey '^[[A' up-line-or-search
+#bindkey '^[[B' down-line-or-search
 
-bindkey '^[[Z' reverse-menu-complete
+#bindkey "^[[H" beginning-of-line
+#bindkey "^[[1~" beginning-of-line
+#bindkey "^[[F"  end-of-line
+#bindkey "^[[4~" end-of-line
+#bindkey ' ' magic-space    # also do history expansion on space
+
+#bindkey '^[[Z' reverse-menu-complete
+
+
+
 
 # consider emacs keybindings:
 
