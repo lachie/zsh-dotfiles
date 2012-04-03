c179b927f45bf39565dcae17bc2d7051581580bc
Lachie Cox <lachiec@gmail.com>
2012-04-03 16:31:27 +1000

    change rbenv path (or it doesn't work when you install rbenv with homebrew)

--- lib/rbenv.zsh
+++ lib/rbenv.zsh
@@ -1,5 +1,5 @@
 function rbenv_prompt_info() {
-  ruby_version=$(~/.rbenv/bin/rbenv version-name 2> /dev/null) || return
+  ruby_version=$(rbenv version-name 2> /dev/null) || return
   echo "($ruby_version)"
 }
 
