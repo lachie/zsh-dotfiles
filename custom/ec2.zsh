5e5722737639478f08f05f19fd6465e0bf4cb917
Lachie Cox <lachiec@gmail.com>
2012-10-16 08:24:43 +1100

    support ubuntu and homebrew

--- custom/ec2.zsh
+++ custom/ec2.zsh
@@ -1,8 +1,17 @@
 export EC2_KEYPAIR_US_EAST_1=default
 
-export JAVA_HOME="$(/usr/libexec/java_home)"
+if [ -x /usr/libexec/java_home ]; then
+  JAVA_HOME="$(/usr/libexec/java_home)"
+else
+  JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+fi
+export JAVA_HOME
 export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
 export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
 
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+HOMEBREW_EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+if [ -d $HOMEBREW_EC2_HOME ]; then
+  export EC2_HOME=$HOMEBREW_EC2_HOME
+fi
+
 export AWS_RDS_HOME="/usr/local/Cellar/rds-command-line-tools/1.3.003/jars"
