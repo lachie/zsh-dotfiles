58e4cb8842ee4b12803725ebaea82543f8a7ab1f
Lachie Cox <lachiec@gmail.com>
2013-02-26 11:14:28 +1100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-ln -sf `pwd`/zshrc $HOME/.zshrc
-ln -sf `pwd`/zshenv $HOME/.zshenv
-ln -sf `pwd` $HOME/.zsh
-chsh -s /bin/zsh
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
+ln -sf $HERE/zshrc $HOME/.zshrc
+ln -sf $HERE/zshenv $HOME/.zshenv
+
+
+ZSH=$(command -v zsh)
+command -v zsh && chsh -s $(command -v zsh)
+
+
+SRC=$HERE/other_files/$(uname)
+for f in $(find $SRC -type f); do
+  dest=$(dirname "$HOME/${f#$SRC}")
+  [ "$HOME" = "$dest" ] || mkdir -p $dest
+  ln -sf $f $dest
+done
