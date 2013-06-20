2b67ff74384193b586276e2f27e53e23e5da6e54
Lachie Cox <lachiec@gmail.com>
2013-06-20 10:46:54 +1000

    use ruby were built against

--- custom/editor.zsh
+++ custom/editor.zsh
@@ -3,13 +3,14 @@ export EDITOR=/usr/local/bin/vim
 function mvim()
 {
   # spawn a sub-shell, unset environment variables for the sub-shell, start mvim. 
+  # match ruby version vim was built against
   (
-    unset GEM_PATH
-    unset GEM_HOME
-    rbenv shell system
     if [ -x $HOME/bin/mvim ]; then
+      rbenv shell system
       command $HOME/bin/mvim "$@"
     else
+      # TODO write this value somewhere?
+      rbenv shell 1.9.3-p327
       command $EDITOR "$@"
     fi
   )
